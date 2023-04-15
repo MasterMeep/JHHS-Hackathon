527794de3f7809fed586dc2d4d68a5bee0064356
TJA_07 <105525318+TJA07@users.noreply.github.com>
2023-04-15 15:18:08 -0700

    Update venmo.py

--- venmo.py
+++ venmo.py
@@ -1,11 +1,14 @@
 from venmo_api import Client
-def email():
+
+def email(name, senderEmail):
     email = ""
-    amout = 0
+    amount = 0
+    id = 0
 
     def transactioninfo():
         global email
         global amount
+        global id
         #access_token = Client.get_access_token(username='rudrappandya@gmail.com', password='SwordsAMillion12')
         access_token = "6acbf7822f859806407f63ae9afb13e2f0d2fc3c068210404cf8722afe545b2c"
         print("Your token (please save):", access_token)
@@ -21,6 +24,8 @@ def email():
                 print(transaction)
                 amount = str(transaction).split(",")[6]
                 email = str(transaction).split(",")[-18]
+                id = str(transaction).split(",")[0]
+
 
             print("\n" + "=" * 15 + "\n\tNEXT PAGE\n" + "=" * 15 + "\n")
             transactions = transactions.get_next_page()
@@ -34,16 +39,18 @@ def email():
     port  =  465
 
     sender   =  'rkrao1144@gmail.com'
-    password = input('enter  your password here')
+    password = "hcduyeqcfpgbfslv"
 
-    receiver = 'rohan.krish.rao@gmail.com'
+    receiver = senderEmail
     message = f"""\
     From: {sender}
     To: {receiver}
     Subject: Project HeadSpace and Timing Donation
 
-    {amount}
-    EOI: 83-3998869
+    {email}\n
+    Donation: {amount}\n
+    From: {name}\n
+    EOI: 83-3998869\n
 
     Basic Info About BH&T: PHAT began operating in 2017 and officially began operating as a 501(C)(3) in 2019. The organization was founded by Eric Peterson after a fellow teammate took his own life after multiple tours overseas. As an organization, we believe that we live in a community that supports their veterans but may not know how to show it, and we have veterans that need that support but may not know how to ask. Our three focuses are: Veterans Advocacy, Veterans Outreach and Veterans Village
 
